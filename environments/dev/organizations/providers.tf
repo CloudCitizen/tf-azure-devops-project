a0c57d58da76304ae61635855f0a8f52e57dc66f
Alex Galani <alex@cloudcitizen.eu>
2024-07-15 14:05:51 +0200

    Fix

--- environments/dev/organizations/providers.tf
+++ environments/dev/organizations/providers.tf
@@ -1,4 +1,3 @@
 provider "azuredevops" {
-  org_service_url       = "https://dev.azure.com/cloudcitizens"
-  personal_access_token = "gu5dedgfr6cdgsa6xcw4yzgd5dvrarpbq56632x3k3jjoxf6y2ra"
+  org_service_url = "https://dev.azure.com/cloudcitizens"
 }
